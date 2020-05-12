e97f4594c510e0fe9024cce0b0f44c9da8a76d83
Bill Glinton <omuomb@gmail.com>
2020-05-12 21:10:57 +0300

    Add files via upload

---
+++ question_1/Main.java
@@ -0,0 +1,47 @@
+/**
+ * @author Bill Glinton
+ * Admission Number:097292
+ * Email:tom.omuom@strathmore.edu
+ */
+
+/**
+ * This is the Main class of the program
+ * The MultiplicationTable Class is called during the program's execution to print
+ * the Multiplication Table of numbers between ranges
+ */
+
+//Main Class
+public class Main {
+    //Main Method
+    public static void main(String[] args) {
+        System.out.println("\n=================================================================================================================================");
+        System.out.println("\n================================================================= Author:           Bill Glinton             ====================\n");
+        System.out.println("================================================================= Admission Number: 097292                   ====================\n");
+        System.out.println("================================================================= Email:            tom.omuom@strathmore.edu ====================\n");
+        System.out.println("=================================================================================================================================\n");
+        System.out.println("=== A Java Program that prints a Multiplication Table of numbers from range to range ============================================\n");
+        System.out.println("=================================================================================================================================\n\n");
+
+        //The MultiplicationTable class is called in Creation of first MultiplicationTable object
+        MultiplicationTable FirstMultiplicationNumber = new MultiplicationTable(3, 17, 25);
+        //The PrintTable() Method of the MultiplicationTable class is called to
+        //print the multiplication table of 3 between 17 and 25
+        System.out.println(FirstMultiplicationNumber.printTable());
+
+        //The MultiplicationTable class is called in Creation of first MultiplicationTable object
+        MultiplicationTable SecondMultiplicationNumber = new MultiplicationTable(17, 3, 49);
+        //The PrintTable() Method of the MultiplicationTable class is called to
+        //print the multiplication table of 3 between 17 and 25
+        System.out.println(SecondMultiplicationNumber.printTable());
+
+        //The MultiplicationTable class is called in Creation of first MultiplicationTable object
+        MultiplicationTable ThirdMultiplicationNumber = new MultiplicationTable(0, 1, 5);
+        //The PrintTable() Method of the MultiplicationTable class is called to
+        //print the multiplication table of 3 between 17 and 25
+        System.out.println(ThirdMultiplicationNumber.printTable());
+
+        System.out.println("================================================= T H E      E N D ==============================================================\n");
+        System.out.println("=================================================================================================================================\n");
+        
+    }
+}
---
+++ question_1/MultiplicationTable.java
@@ -0,0 +1,65 @@
+/**
+ * @author Bill Glinton
+ * Admission Number: 097292
+ * Email           : tom.omuom@strathmore.edu
+ */
+
+/**
+ * The Written program prints multiplication tables of 
+ * a number from range to range
+ */
+public class MultiplicationTable {
+    //Attributes
+    int multiplication_number;//Multiplication Number
+    int minimum_range;//Lowest value 
+    int maximum_range;//Highest Value
+
+    // Constructor
+    public MultiplicationTable(int num, int min, int max) {
+        multiplication_number = num;
+        minimum_range = min;
+        maximum_range = max;
+    }
+
+    // Getters
+    public int getMultiplication_Number() {
+        return multiplication_number;
+    }
+
+    public int getMinimum_Range() {
+        return minimum_range;
+    }
+
+    public int getMaximum_Range() {
+        return maximum_range;
+    }
+
+    // Setters
+    public void setMultiplication_Number(int newMultiplicationNumber) {
+        this.multiplication_number = newMultiplicationNumber;
+    }
+
+    public void setMinimumR_Range(int newMinimumRange) {
+        this.minimum_range = newMinimumRange;
+    }
+
+    public void setMaximum_Range(int newMaximumRange) {
+        this.maximum_range = newMaximumRange;
+    }
+
+    // Method to print the Multiplication Table
+    public String printTable() {
+        System.out.println("=================================================================================================================================\n");
+        System.out.println("=================================================================================================================================\n");
+        System.out.println("=== Printing the Multiplcation Table of " + multiplication_number + " between "
+                + minimum_range + " and " + maximum_range + "\n");
+        System.out.println("=================================================================================================================================\n");
+        System.out.println("=================================================================================================================================\n");
+        for (int j = minimum_range; j <= maximum_range; j++) {
+            System.out.println(multiplication_number + " X " + j + " = " + multiplication_number * j + "\n");
+        }
+        String string = "\n=================================================================================================================================\n";
+        return string;
+    }
+
+}
